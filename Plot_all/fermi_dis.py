1bb75defdc0b9a5a207d963725d2e04d239528d7
Pawel <pawel@pawel-pc.home>
2017-01-28 18:44:12 +0100

    add subplot

--- Plot_all/fermi_dis.py
+++ Plot_all/fermi_dis.py
@@ -16,16 +16,25 @@ def fun_fermi(E, kT):
     E_f = 0.55
     return 1/( 1 + np.exp((E-E_f)/kT))
 
-E = np.linspace(0, 1, 1000)
+E = np.linspace(0, 1.2, 1000)
 fermi_10 = fun_fermi(E, 0.0086)
 fermi_300 = fun_fermi(E, 0.0259)
 fermi_450 = fun_fermi(E, 0.039)
 
-ax1 = plt.subplot()
-ax1.plot(E, fermi_10, 'b-', label="10\,K", linewidth=2)
-ax1.plot(E, fermi_300, 'g-', label="300\,K", linewidth=2)
-ax1.plot(E, fermi_450, 'r-', label="450\,K", linewidth=2)
+ax1 = plt.subplot(121)
+ax1.plot(fermi_10, E,'b-', label="10\,K", linewidth=2)
+ax1.plot(fermi_300, E,'g-', label="300\,K", linewidth=2)
+ax1.plot(fermi_450, E, 'r-', label="450\,K", linewidth=2)
+ax1.plot([-0.1, 1.1], [0.55, 0.55], 'k--', linewidth=3)
+ax1.text(0.15, 0.48, "Poziom fermiego")
+ax1.set_xlim([-0.1, 1.1])
+ax1.set_xlabel("prawdopodobieństwa ")
+ax1.set_ylabel("energia [eV]")
+
 plt.legend()
 plt.grid(True)
+
+ax1 = plt.subplot(122)
+ax1.plot(E, fermi_10, label="10\,K", linewidth=2)
 
 plt.show()
